5080c1cde64e6fa0d25a612c4d318cbd77fd55bb
Mateus <Maia>
2020-09-30 17:54:07 +0100

    Adding some tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,9 +1,43 @@
 using jubart
-using Test1
+using Test
+
+@test isa(sim_friedman_simple(100,5,0.01),Dict)
+
+aux=sim_friedman_simple(100,5,0.01)
+aux_model=juBart(aux["x"],aux["y"],2)
 
 @testset "jubart.jl" begin
-    # 2x + 3y
-    @test my_f(2,1) == 7
-    @test my_f(2,3) == 13
-    @test my_f(1,3) == 11
+    # Testing the friedman_simulation_function
+    @test isa(sim_friedman_simple(10,5,0.01),Dict)
+    @test isa(sim_friedman_simple(10,5,0.01)["x"],Array{Float64,2})
+    @test isa(sim_friedman_simple(10,5,0.01)["y"],Array{Float64,1})
+    @test isa(sim_friedman_simple(10,5,0.01)["true_mean"],Array{Float64,1})
+
+    # Testing the Julia model
+    data_test=sim_friedman_simple(100,5,0.001)
+    min_node_size_test=10
+    number_tree=5
+    model_test=juBart(data_test["x"],data_test["y"],number_tree,min_node_size_test)
+
+    # Testing just the output
+    @test isa(model_test,Dict)
+
+    # Test for check the min node size
+    node_size_count=0 # Counter of min_node_size
+
+    for mh_tree in model_test["trees"]
+        for m in 1:number_tree
+            tree_nodes=mh_tree[m].nodes
+            for run_tree_nodes in tree_nodes
+                if(length(run_tree_nodes.observations_index)<min_node_size_test)
+                    node_size_bol+=1
+                end
+            end
+        end
+    end
+
+    if(node_size_count==0) node_size_bol=true else node_size_bol=false end
+
+    @test node_size_bol
+
 end
